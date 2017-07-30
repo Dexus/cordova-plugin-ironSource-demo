692ad382b20468ebd591834f1c3eb67c4d7c61ad
Josef Fröhle <github@josef-froehle.de>
2017-07-30 17:17:09 +0200

    Initial working commit

--- www/js/index.js
+++ www/js/index.js
@@ -1,118 +1,159 @@
 document.addEventListener('deviceready', function onDeviceReady() {
 
-	var ANDROID_KEY = '3f09a0c9';
-	var IOS_KEY = '3efd0b09';
-	var ssAds = new IronSourceAds(cordova.platformId === 'ios' ? IOS_KEY : ANDROID_KEY, 'demo_' + Date.now());
-	var eventLog = document.getElementById('eventLog');
-	var interstitialBtn = document.getElementById('interstitial');
-	var offerwallBtn = document.getElementById('offerWall');
-	var rewardedVideoBtn = document.getElementById('rewarded');
-	var rvAvailable = false;
-	var interstitialAvailable = false;
+  var ANDROID_KEY = '5d969e95';
+  var IOS_KEY = '5d969e95';
+  var eventLog = document.getElementById('eventLog');
+  var interstitialBtn = document.getElementById('interstitial');
+  var offerwallBtn = document.getElementById('offerWall');
+  var rewardedVideoBtn = document.getElementById('rewarded');
+  var rvAvailable = false;
+  var IronSourceAdsHelper = {
+    "ANDROID_KEY": '5d969e95',
+    "IOS_KEY": '5d969e95',
+    "rvAvailable": false,
+    "interstitialAvailable": false,
+    "adsLoadFailedInterstitial": 0,
+  };
+  var ssAds = new IronSourceAds(cordova.platformId === 'ios' ? IOS_KEY : ANDROID_KEY, 'demo_' + Date.now(), function() {
 
-	window.addEventListener("interstitialInitialized", function() {
-		log("interstitialInitialized");
-	}, false);
+    ssAds.loadInterstitial();
 
-	window.addEventListener("interstitialInitizationFailed", function(e) {
-		log("interstitialInitFailed", e.error);
-	}, false);
+    window.addEventListener("onInterstitialAdReady", function() {
+      log("onIntersitialAdReady: ", arguments);
+      IronSourceAdsHelper.interstitialAvailable = true;
+      IronSourceAdsHelper.adsLoadFailedInterstitial = 0;
+      interstitialBtn.classList.remove('disabled');
+    }, false);
 
-	window.addEventListener("interstitialAvailabilityChanged", function(e) {
-		log("interstitialAvailabilityChanged", e.available);
-		interstitialAvailable = e.available;
-		if(e.available) {
-			interstitialBtn.classList.remove('disabled');
-		} else {
-			interstitialBtn.classList.add('disabled');
-		}
-	}, false);
-	window.addEventListener("interstitialShown", function() {
-		log("interstitialShown")
-	}, false);
-	window.addEventListener("interstitialShowFailed", function(e) {
-		log("interstitialShowFailed", e.error);
-	}, false);
-	window.addEventListener("interstitialClicked", function() {
-		log("interstitialClicked")
-	}, false);
-	window.addEventListener("interstitialClosed", function() {
-		log("interstitialClosed")
-	}, false);
-	window.addEventListener("offerwallClosed", function() {
-		log("offerwallClosed")
-	}, false);
-	window.addEventListener("offerwallCreditFailed", function(e) {
-		log("offerwallCreditFailed", e.error);
-	}, false);
-	window.addEventListener("offerwallCreditReceived", function(e) {
-		log("offerwallCreditReceived", e.credit)
-	}, false);
-	window.addEventListener("offerwallShowFailed", function(e) {
-		log("offerwallShowFailed", e.error);
-	}, false);
-	window.addEventListener("offerwallOpened", function() {
-		log("offerwallOpened");
-	}, false);
-	window.addEventListener("offerwallInitializationFailed", function(e) {
-		log("offerwallInitializationFailed", e.error);
-	}, false);
-	window.addEventListener("offerwallInitialized", function() {
-		log("offerwallInitialized");
-	}, false);
-	window.addEventListener("rewardedVideoRewardReceived", function(e) {
-		log("rewardedVideoRewardReceived", e.placement);
-	}, false);
-	window.addEventListener("rewardedVideoEnded", function() {
-		log("rewardedVideoEnded");
-	}, false);
-	window.addEventListener("rewardedVideoStarted", function() {
-		log("rewardedVideoStarted")
-	}, false);
-	window.addEventListener("rewardedVideoAvailabilityChanged", function(e) {
-		log("rewardedVideoAvailabilityChanged", e.available);
-		rvAvailable = e.available;
-		if(e.available) {
-			rewardedVideoBtn.classList.remove('disabled');
-		} else {
-			rewardedVideoBtn.classList.add('disabled');
-		}
-	}, false);
-	window.addEventListener("rewardedVideoClosed", function() {
-		log("rewardedVideoClosed");
-	}, false);
-	window.addEventListener("rewardedVideoOpened", function() {
-		log("rewardedVideoOpened");
-	}, false);
-	window.addEventListener("rewardedVideoInitializationFailed", function(e) {
-		log("rewardedVideoInitializationFailed", e.error);
-	}, false);
-	window.addEventListener("rewardedVideoInitialized", function() {
-		log("rewardedVideoInitialized");
-	}, false);
+    window.addEventListener("onInterstitialAdLoadFailed", function(e) {
+      log("onInterstitialAdLoadFailed", e);
+      interstitialBtn.classList.add('disabled');
+      if (IronSourceAdsHelper.adsLoadFailedInterstitial <= 5) {
+        IronSourceAdsHelper.adsLoadFailedInterstitial++;
+        var timer = (IronSourceAdsHelper.adsLoadFailedInterstitial + 1) * 1500;
+        setTimeout(ssAds.loadInterstitial, timer);
+      }
+    }, false);
 
-	rewardedVideoBtn.addEventListener('click', function reward() {
-		if(rvAvailable) {
-			ssAds.showRewardedAd();
-		} else {
-			alert('No rewarded videos are available.')
-		}
-	}, false);
+    window.addEventListener("onInterstitialAdOpened", function() {
+      log("onInterstitialAdOpened")
+      interstitialAvailable = false;
+      interstitialBtn.classList.add('disabled');
+    }, false);
 
-	offerwallBtn.addEventListener('click', function offerWall() {
-		ssAds.showOfferwall();
-	}, false);
+    window.addEventListener("onInterstitialAdClicked", function() {
+      log("onInterstitialAdClicked");
+      IronSourceAdsHelper.interstitialAvailable = false;
+      interstitialBtn.classList.add('disabled');
+    }, false);
 
-	interstitialBtn.addEventListener('click', function interstitial() {
-		if(interstitialAvailable) {
-			ssAds.showInterstitial();
-		} else {
-			alert('No interstitial ads available.')
-		}
-	}, false);
+    window.addEventListener("onInterstitialAdClosed", function() {
+      log("onInterstitialAdClosed")
+      IronSourceAdsHelper.interstitialAvailable = false;
+      interstitialBtn.classList.add('disabled');
+      ssAds.loadInterstitial();
+    }, false);
 
-	function log(event, data) {
+    window.addEventListener("onInterstitialAdShowSucceeded", function() {
+      log("onInterstitialAdShowSucceeded")
+    }, false);
 
-		eventLog.value += [event, data !== undefined ? (': ' + JSON.stringify(data)) : '', '\n'].join('');
-	}
+
+    window.addEventListener("onInterstitialAdShowFailed", function(e) {
+      log("onInterstitialAdShowFailed", e)
+      IronSourceAdsHelper.interstitialAvailable = false;
+    }, false);
+
+    window.addEventListener("onOfferwallOpened", function() {
+      log("onOfferwallOpened");
+    }, false);
+
+    window.addEventListener("onOfferwallClosed", function() {
+      log("onOfferwallClosed")
+    }, false);
+
+    window.addEventListener("onOfferwallAdCredited", function(e) {
+      log("onOfferwallAdCredited", e)
+    }, false);
+
+    window.addEventListener("onGetOfferwallCreditsFailed", function(e) {
+      log("onGetOfferwallCreditsFailed", e);
+    }, false);
+
+    window.addEventListener("onOfferwallShowFailed", function(e) {
+      log("onOfferwallShowFailed", e);
+    }, false);
+
+    window.addEventListener("onOfferwallAvailable", function(e) {
+      log("onOfferwallAvailable", e);
+      IronSourceAdsHelper.owAvailable = e.offerAvailable;
+      if (IronSourceAdsHelper.owAvailable) {
+        offerwallBtn.classList.remove('disabled');
+      } else {
+        offerwallBtn.classList.add('disabled');
+      }
+    }, false);
+
+    window.addEventListener("onRewardedVideoAdRewarded", function(e) {
+      log("onRewardedVideoAdRewarded", e);
+    }, false);
+
+    window.addEventListener("onRewardedVideoAdEnded", function() {
+      log("onRewardedVideoAdEnded");
+    }, false);
+
+    window.addEventListener("onRewardedVideoAdStarted", function() {
+      log("onRewardedVideoAdStarted")
+    }, false);
+
+    window.addEventListener("onRewardedVideoAvailabilityChanged", function(e) {
+      log("rewardedVideoAvailabilityChanged", e);
+      IronSourceAdsHelper.rvAvailable = e.videoAvailable;
+      if (IronSourceAdsHelper.rvAvailable) {
+        rewardedVideoBtn.classList.remove('disabled');
+      } else {
+        rewardedVideoBtn.classList.add('disabled');
+      }
+    }, false);
+
+    window.addEventListener("onRewardedVideoAdClosed", function() {
+      log("onRewardedVideoAdClosed");
+    }, false);
+    window.addEventListener("onRewardedVideoAdOpened", function() {
+      log("onRewardedVideoAdOpened");
+    }, false);
+
+    window.addEventListener("onRewardedVideoAdShowFailed", function(e) {
+      log("onRewardedVideoAdShowFailed", e);
+      IronSourceAdsHelper.rvAvailable = false;
+    }, false);
+
+    rewardedVideoBtn.addEventListener('click', function reward() {
+      if (IronSourceAdsHelper.rvAvailable) {
+        ssAds.showRewardedVideo();
+      } else {
+        alert('No rewarded videos are available.')
+      }
+    }, false);
+
+    offerwallBtn.addEventListener('click', function offerWall() {
+      if (IronSourceAdsHelper.owAvailable) {
+        ssAds.showOfferwall();
+      } else {
+        alert('No Offerwall ads available.')
+      }
+    }, false);
+
+    interstitialBtn.addEventListener('click', function interstitial() {
+      if (IronSourceAdsHelper.interstitialAvailable) {
+        ssAds.showInterstitial();
+      } else {
+        alert('No interstitial ads available.')
+      }
+    }, false);
+
+    function log(event, data) {
+      eventLog.value += [event, data !== undefined ? (': ' + JSON.stringify(data)) : '', '\n'].join('');
+    }
+  });
 }, false);
